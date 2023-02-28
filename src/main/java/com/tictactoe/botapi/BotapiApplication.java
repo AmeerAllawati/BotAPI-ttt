016e797b9da2ba8d3cd6827d82b54eb151b2374d
AmeerAllawati <amaab@ucdavis.com>
2023-02-28 12:08:39 +0400

    Initial commit

---
+++ src/main/java/com/tictactoe/botapi/BotapiApplication.java
@@ -0,0 +1,13 @@
+package com.tictactoe.botapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tictactoe/botapi/BotapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tictactoe.botapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
